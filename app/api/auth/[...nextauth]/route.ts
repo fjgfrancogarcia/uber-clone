065e0d4e04732bab03623f725985e1f860222e98
fjgfrancojg <fjgfrancojg@gmail.com>
2025-04-03 04:23:28 -0300

    Simplificar implementación de NextAuth para corregir errores de compilación

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,6 +1,7 @@
-import NextAuth from "next-auth";
-import { authOptions } from "../../../../auth";
+import NextAuth from 'next-auth';
+import { authOptions } from '../../../../auth';
 
+// Crear y exportar directamente las funciones GET y POST
 const handler = NextAuth(authOptions);
-
-export { handler as GET, handler as POST }; 
+export const GET = handler;
+export const POST = handler; 
--- auth.ts
+++ auth.ts
@@ -1,4 +1,3 @@
-import NextAuth from 'next-auth';
 import CredentialsProvider from 'next-auth/providers/credentials';
 import { prisma } from './prisma/prisma';
 import bcrypt from 'bcrypt';

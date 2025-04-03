97cd66897682f68e5fa8d3991220df2badb51ba7
fjgfrancojg <fjgfrancojg@gmail.com>
2025-04-03 02:19:17 -0300

    Actualizar componentes puente para Map y RideRequest

---
+++ src/app/components/Map.tsx
@@ -0,0 +1,8 @@
+'use client';
+
+// Este archivo es un puente para redirigir a la implementación real
+// Importa el Map real desde la ubicación correcta y lo reexporta
+import MapImpl from '../../../src/components/Map';
+
+// Re-exportar el componente
+export default MapImpl; 
---
+++ src/app/components/RideRequest.tsx
@@ -0,0 +1,8 @@
+'use client';
+
+// Este archivo es un puente para redirigir a la implementación real
+// Importa el RideRequest real desde la ubicación correcta y lo reexporta
+import RideRequestImpl from '../../../src/components/RideRequest';
+
+// Re-exportar el componente
+export default RideRequestImpl; 
